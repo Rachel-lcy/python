08716abe91ceda634bedc04a1f9530acbe5777ef
Rachel <hurui.lcy@gmail.com>
2025-04-28 23:18:25 -0600

    container

---
+++ containers.py
@@ -0,0 +1,22 @@
+#container setup
+# a_tuple = (1,2,3, 'a string')
+# # print(a_tuple)
+# a_list = [1,2,3, 'a string',2]
+
+# # a_list.append('another word')
+# print(a_list)
+# a_set = {1,2,3 ,4,4}
+# print(set(a_list))#set() without duplicated value 
+
+# a_dictionary = {'key': 'value', 123: [1,2,3]}
+# a_dictionary['new key'] = 1.2
+# print(a_dictionary)
+
+# how to get values from a container
+# user_list = ['Lisa', 'Bob', 'Alex', 'Anna', 'John']
+# print(user_list[0:3:2])
+# print(user_list[start:end:step])
+
+#exercise
+exercise_list = [1,2,3,4,5,6,7,8,9,10]
+print(exercise_list[7::-2])
