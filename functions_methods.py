696c63a05d24e9309f48e05e47019145f1e05094
Rachel <hurui.lcy@gmail.com>
2025-04-28 21:26:30 -0600

    Methods & Functions

---
+++ functions_methods.py
@@ -0,0 +1,26 @@
+#functions
+# print('a value')
+# print(input('ask for a value'))
+
+#methods -> functions of datatypes
+# print('value'.upper())
+# print('NUMBER'.lower())
+# print('value'.replace('e', '3'))
+
+
+# new functions
+# print(abs(-99)) # absolute value - turns any number into a positive number
+# print(max(10,99))
+# print(min(0,99))
+# print(len('methodology'))
+
+#exercise-pythagoras theorem
+side_a = int(input('The width of the triangle:'))
+side_b = int(input('The height of the triangle:'))
+hypotenuse = (pow(side_a, 2) + pow(side_b, 2)) ** 0.5
+print('The hypotenuse is:  ', hypotenuse)
+print('The hypotenuse is:  ', round(hypotenuse, 2))
+# print(type(int(side_a)))
+# print(type(int(side_b)))
+
+
