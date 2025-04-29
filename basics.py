37c03f28f943be272883706554c7c3da5a72a074
Rachel <hurui.lcy@gmail.com>
2025-04-28 20:08:00 -0600

    python basics

---
+++ basics.py
@@ -0,0 +1,38 @@
+# execution order
+print('line 1') # this is will be ignored
+print('line 2')
+print('line 3')
+
+#datatypes
+print('words') #strings
+print('words')
+print(123) # numbers-integers/ints
+print(-10)
+print(1.5) # floating point value/float
+
+# operations
+print(3+3)
+print(3-3)
+print(3*3)
+print(3/3)
+print(10 + 0.17384943)
+# print(10 + 'hello')
+print(10 * 'hello')
+print(13-3*4 +2)
+
+
+#variables
+test_value = 123
+# test = test + 50
+test_value += 50
+
+print(test_value)
+
+#input
+# input("Please write something")
+# user_input = input("Please write something")
+# print(user_input)
+
+#exercise
+user_name = input("What's your name?")
+print('Hello, Welcome', user_name, 'Have a nice day!')
