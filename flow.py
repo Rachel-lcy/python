08f3b2a6d4f485e7f96a11e8a406a2c39a14d1f9
Rachel <hurui.lcy@gmail.com>
2025-04-29 20:54:33 -0600

    Changing the flow of the codē

---
+++ flow.py
@@ -0,0 +1,57 @@
+# print(5 == 4)
+# if statement
+# if 3 >4:
+#   print('correct result')
+# elif 0>1:
+#   print('some other result')
+# elif 0 ==0 and 5 >1:
+#   print('some correct result')
+#   if len([1,2,3]) > 2:
+#     print('list is long enough')
+# else:
+#   print('incorrect result')
+
+
+#while loop
+# counter = 0
+# while counter <= 10:
+#   print(counter)
+#   counter += 1
+#   if counter == 5:
+#     print('counter is 5')
+# print('while loop has finished')
+
+#for loop
+# test_list = [1,2,3,4,5]
+# for x in test_list:
+#   print(x)
+
+
+# test_list1 = {1:2, 3:4, 5:6}
+# # for x in test_list1.keys():
+# # for x in test_list1.values():
+# for x in test_list1.items():
+#   print(x[0])
+
+#truthy and falsy
+# if '':
+#   print('truthy')
+# else:
+#   print('falsy')
+
+
+#exercise
+exercise_list = [1,2,3,4,5]
+for X in exercise_list:
+  if X == 2:
+    print('The value is 2')
+  else:
+    print('The value is not 2')
+
+exercise_counter = 0
+while exercise_counter <6:
+  print('last item')
+  exercise_counter +=1
+
+
+
